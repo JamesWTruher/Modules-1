a4cab4b8ecf6c151fc2c09933f554306db9aeb6f
James Truher <jimtru@microsoft.com>
2020-03-19 16:18:36 -0700

    split command class into command and commandbase, rather than creating a subcommand class

--- Modules/Microsoft.PowerShell.KubeCtl/experimental/ParseKubectlHelp.ps1
+++ Modules/Microsoft.PowerShell.KubeCtl/experimental/ParseKubectlHelp.ps1
@@ -1,6 +1,6 @@
-$GeneralTopicTokens = @("Usage:","Commands.*:$")
+$CommandTopicTokens = @("Commands.*:$")
+$UsageTopicTokens = "Usage:"
 $GroupTopicTokens   = @()
-$CommandTopicTokens = @()
 
 class Topic {
     [string]$Usage
@@ -9,17 +9,41 @@ class Topic {
     [string]$Description
 }
 
-class Command {
+
+class CommandBase {
     [string]$Command
     [string]$Description
-    [ParameterInfo[]]$Parameters
     hidden [string]$originalText
-    Command ([string]$text ) {
+
+    CommandBase ([string]$text ) {
         $c,$d = "$text".Trim().Split("  ", 2, [System.StringSplitOptions]::RemoveEmptyEntries) | %{"$_".Trim()}
         $this.Command = $c
         $this.Description = $d
         $this.originalText = $text
     }
+}
+
+class Command : CommandBase {
+    [ParameterInfo[]]$Parameters
+
+    Command([string]$text) : base($text) { }
+
+    [string]CreateParamStatement() {
+        return "param (`n{0} )" -f $this.Parameters.Foreach({$_.ToString}) -join ",`n"
+    }
+
+    <#
+    [string]CreateProxyFunction() {
+        $s = .{
+            'function Invoke-Kube{0}' -f $this.Name
+            '{'
+            $this.CreateParamStatement()
+            'kubectl ' + $this.Command + ' -o json'
+            '}'
+        $sb
+
+    }
+    #>
 }
 
 class GroupCommandInfo {
@@ -28,6 +52,13 @@ class GroupCommandInfo {
 }
 
 # note that for PowerShell, we won't have parameter aliases
+# An option takes the shape of '--<name>=<defaultvalue>: <description>"
+# it can also look like '-<n>, --<name>=<defaultvalue>: <description>" where '-<n>' is an option alias
+# we through away the aliases
+# The <defaultvalue> might be something that we can interpret, so try
+# also, some options can be converted to powershell switches (their default value is True or False).
+# if the default value is 'True',
+# convert that to "No<name>" when building the string which represents the option
 class ParameterInfo {
     [string]$Name
     [string]$Description
@@ -41,7 +72,7 @@ class ParameterInfo {
         if ( $text -match ".* --(?<option>.*): (?<Description>.*)" ) {
             $pname,$default = $matches['option'] -split "="
             $pDefaultValue = $default.Trim("'")
-            $this.Name = "${pname}".Trim() -replace "-(.)",{($_ -replace "-").ToUpper()} -replace "^(.)",{"$_".ToUpper()}
+            $this.Name = "${pname}" # .Trim() -replace "-(.)",{($_ -replace "-").ToUpper()} -replace "^(.)",{"$_".ToUpper()}
             $this.Description = $matches['Description'].Trim()
             $this.IsMandatory = $isMandatory
             $this.Parsed = $true
@@ -81,20 +112,20 @@ class ParameterInfo {
             if ( $this.DefaultValue ) {
                 $pName = "No${pName}"
             }
-            $pString = '[Parameter(Mandatory=${0})][switch]${1}' -f $this.IsMandatory,$pName
+            $pString = '[Parameter(Mandatory=${0})][switch]${{{1}}}' -f $this.IsMandatory,$pName
         }
         elseif ( $this.ValueType -eq [array] -and ! $this.DefaultValue ) {
-                $pString = '[Parameter(Mandatory=${0})][{1}]${2} = @()' -f $this.IsMandatory,$this.ValueType,$this.Name
+                $pString = '[Parameter(Mandatory=${0})][{1}]${{{2}}} = @()' -f $this.IsMandatory,$this.ValueType,$this.Name
         }
         elseif ( $this.ValueType -eq [string] -and $this.DefaultValue ) {
-                $pString = '[Parameter(Mandatory=${0})][{1}]${2} = "{3}"' -f $this.IsMandatory,$this.ValueType,$this.Name,$this.DefaultValue
+                $pString = '[Parameter(Mandatory=${0})][{1}]${{{2}}} = "{3}"' -f $this.IsMandatory,$this.ValueType,$this.Name,$this.DefaultValue
             
         }
         elseif ( $this.DefaultValue ) {
-                $pString = '[Parameter(Mandatory=${0})][{1}]${2} = {3}' -f $this.IsMandatory,$this.ValueType,$this.Name,$this.DefaultValue
+                $pString = '[Parameter(Mandatory=${0})][{1}]${{{2}}} = {3}' -f $this.IsMandatory,$this.ValueType,$this.Name,$this.DefaultValue
         }
         else {
-            $pString = '[Parameter(Mandatory=${0})][{1}]${2}' -f $this.IsMandatory,$this.ValueType,$this.Name
+            $pString = '[Parameter(Mandatory=${0})][{1}]${{{2}}}' -f $this.IsMandatory,$this.ValueType,$this.Name
         }
         return $pString
     }
@@ -148,26 +179,89 @@ function Invoke-Kubectl {
     [CmdletBinding()]
     param ( [Parameter(ValueFromRemainingArguments,Position=0,Mandatory=$true)][string[]]$command )
     $p = "kubectl $($command -join ' ')"
-    show-prog -message $p
+    show-prog -message "$p"
     kubectl $command
 }
 
 function Get-Options {
     $text = Invoke-Kubectl "options" | ?{$_ -match " --"}
-    return $text
+    $text.Foreach({[ParameterInfo]::new($_, $false)})
+}
+
+function Parse-Command
+{
+    #[CmdletBinding()]
+    #param ( [Parameter(Mandatory=$true,Position=0)][string[]]$text, [Parameter(Mandatory,Position=1)][string[]]$pattern )
+    # param ( $text, $pattern )
+    param ( [array]$text, [array]$pattern, [switch]$Simple )
+    $patternRegex = $pattern -join "|"
+    $results = [System.Collections.ArrayList]::new()
+    for ( $i = 0; $i -lt $text.Count; $i++ ) {
+        if ( $text[$i] -match $patternRegex ) {
+            $i++
+            while ( $text[$i].Trim() -ne "" ) {
+               if ( $Simple ) {
+                   $null = $results.Add([CommandBase]::new($text[$i]))
+               }
+               else
+               {
+                   $null = $results.Add([Command]::new($text[$i]))
+               }
+               $i++
+               if ( $i -gt $text.count ) { break }
+            }
+        }
+    }
+    $results
+}
+
+function Parse-Usage
+{
+    #[CmdletBinding()]
+    #param ( [Parameter(Mandatory=$true,Position=0)][string[]]$text, [Parameter(Mandatory,Position=1)][string[]]$pattern )
+    param ( $text, $pattern )
+    # param ( [array]$text, [array]$pattern )
+    $patternRegex = $pattern -join "|"
+    $results = [System.Collections.ArrayList]::new()
+    for ( $i = 0; $i -lt $text.Count; $i++ ) {
+        if ( $text[$i] -match $patternRegex ) {
+            $i++
+            while ( ($text[$i] -as [string]).Trim() -ne "" ) {
+               $null = $results.Add($text[$i].Trim())
+               $i++
+               if ( $i -gt $text.count ) { break }
+            }
+        }
+    }
+    return $results
 }
 
 function Parse-GeneralHelp
 {
     # first generate the output
-    $text = Invoke-Kubectl "--help"
+    $text = kubectl --help  # Invoke-Kubectl "--help"
+    $usage = Parse-Usage -text $text -pattern $UsageTopicTokens
+    write-verbose -verbose $usage
+    $subcommands = Parse-Command -text $text -pattern $CommandTopicTokens -Simple
+    $r = Get-Options
+    [pscustomobject]@{
+        Subcommands = $subcommands
+        Usage = $usage
+        Examples = @()
+        Options = $r
+    }
 }
-
 
 function parse-helppage
 {
     param ( $text )
+    $parameters = Parse-Options $text
+    $examples = Parse-Examples $text
+    $subcommands = Parse-Command $text ${GeneralTopicTokens}
+    $usage = Parse-Usage $text
 }
 
-$r = Get-Options
-$r.Foreach({[ParameterInfo]::new($_, $false)})
+#$r = Get-Options
+#$r.Foreach({[ParameterInfo]::new($_, $false)})
+$r = Parse-GeneralHelp
+$r
